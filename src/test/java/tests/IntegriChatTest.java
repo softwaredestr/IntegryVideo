94b3bd2a7e60e133e16ee309ccc7f2e926be550f
evgeny <eugeny.noskov@gmail.com>
2020-01-15 18:03:14 +0300

    all sending message tests

--- src/test/java/tests/IntegriChatTest.java
+++ src/test/java/tests/IntegriChatTest.java
@@ -17,8 +17,55 @@ public class IntegriChatTest extends BaseTest {
         chat.typeMessage("blalala");
         chat.sendMessageUsingButton();
         chat.messageShouldContainText("blalala", 2);
+
+        chat.typeMessage("qwertyui");
+        chat.sendMessageUsingEnter();
+        chat.messageShouldContainText("qwertyui", 3);
+
+        chat.typeMessage("https://dima-super-qa.jimdofree.com/");
+        chat.sendMessageUsingEnter();
+        chat.messageShouldContainText("https://dima-super-qa.jimdofree.com/", 4);
+
+        chat.typeMessage("Praesent condimentum consequat mi, sit amet venenatis nisl maximus sit amet. Donec sem mi, sagittis nec fringilla quis, pretium eu dui." +
+                "                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nisl felis, tincidunt nec volutpat non, maximus vitae urna." +
+                "                    Sed mi lacus, commodo at semper vel, vestibulum sit amet sem. Duis a semper nibh." +
+                "                    Mauris arcu purus, pulvinar id commodo vitae, aliquam at nibh. Cras rutrum ac nibh ut faucibus." +
+                "                    In aliquam accumsan sapien, at dignissim elit maximus vel. Aliquam fringilla pellentesque leo at sagittis." +
+                "                    Vivamus velit felis, fermentum in accumsan vel, cursus mattis lorem. Nunc congue tellus ligula. Vivamus sapien velit, rutrum ut hendrerit sed, gravida interdum purus. Nullam et nisl maximus, feugiat leo vel, pretium urna. Nam placerat urna a ipsum vestibulum ultrices." +
+                "                    Phasellus in dolor lorem. Nulla sodales vehicula vestibulum. Donec malesuada malesuada dolor." +
+                "                    Phasellus tristique justo ac quam lacinia luctus. Nam volutpat.");
+        chat.sendMessageUsingEnter();
+        chat.messageShouldContainText("Praesent condimentum consequat mi, sit amet venenatis nisl maximus sit amet. Donec sem mi, sagittis nec fringilla quis, pretium eu dui." +
+                "                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nisl felis, tincidunt nec volutpat non, maximus vitae urna." +
+                "                    Sed mi lacus, commodo at semper vel, vestibulum sit amet sem. Duis a semper nibh." +
+                "                    Mauris arcu purus, pulvinar id commodo vitae, aliquam at nibh. Cras rutrum ac nibh ut faucibus." +
+                "                    In aliquam accumsan sapien, at dignissim elit maximus vel. Aliquam fringilla pellentesque leo at sagittis." +
+                "                    Vivamus velit felis, fermentum in accumsan vel, cursus mattis lorem. Nunc congue tellus ligula. Vivamus sapien velit, rutrum ut hendrerit sed, gravida interdum purus. Nullam et nisl maximus, feugiat leo vel, pretium urna. Nam placerat urna a ipsum vestibulum ultrices." +
+                "                    Phasellus in dolor lorem. Nulla sodales vehicula vestibulum. Donec malesuada malesuada dolor." +
+                "                    Phasellus tristique justo ac quam lacinia luctus. Nam volutpat.", 5);
+
+        chat.typeMessage("<html><body><p>test</p></body></html>");
+        chat.sendMessageUsingEnter();
+        chat.messageShouldContainText("test", 6);
+
+        chat.openPage();
+        chat.sendMultipleMessage(11, "asdf");
+        chat.verifyTrialMessage();
+
+
+
         //input
         //send
         //validate
     }
+                @Test
+    public void editDeleteMessageTest(){
+        chat = new IntegriChatPage(driver);
+                    chat.openPage();
+                    chat.typeMessage("1234");
+                    chat.sendMessageUsingEnter();
+                    chat.editMessage("qwer");
+                    chat.messageShouldContainText("qwer", 1);
+
+                }
 }
