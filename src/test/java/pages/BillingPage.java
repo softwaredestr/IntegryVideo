cde9d2c66e15f63cc383329466ac5b1e5e13688d
evgeny <eugeny.noskov@gmail.com>
2020-01-28 17:49:03 +0300

    Added Billing

--- src/test/java/pages/BillingPage.java
+++ src/test/java/pages/BillingPage.java
@@ -1,18 +1,62 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 public class BillingPage extends BasePage {
-    By billingForm = By.className("container-fluid");
-    By addNewBtn = By.className("btn");
+    By creditCardForm = By.id("credit-card");
+    By addNewBtn = By.xpath("//*[contains(text(),'Add new')]");
     By cardInput = By.className("form-control");
-    By addBtn = By.className("btn");
+    By addBtn = By.xpath("//button[@class='btn']");
     By addedCards = By.className("cards");
     By message = By.className("message");
-    By payPal = By.
+    By payPal = By.className("paypal-button-label-container");
+
 
     public BillingPage(WebDriver driver) {
         super(driver);
     }
+    public BillingPage openPage(){
+        driver.get("https://dev.integrivideo.com/app/billing");
+        return this;
+    }
+    public BillingPage addNewCard(){
+        driver.findElement(addNewBtn).click();
+        return this;
+    }
+    public BillingPage addClick(){
+        driver.findElement(addBtn).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(creditCardForm)));
+        return this;
+    }
+    public BillingPage isPageOpened(){
+        try {
+            wait.until(ExpectedConditions.visibilityOf(driver.findElement(creditCardForm)));
+        } catch(TimeoutException ex) {
+            throw new TimeoutException("Страница не загрузилась");
+        }
+        return this;
+    }
+    public BillingPage fillFields(String cardNum, String month,String year, String name){
+        driver.findElements(cardInput).get(0).click();
+        driver.findElements(cardInput).get(0).sendKeys(cardNum);
+        driver.findElements(cardInput).get(1).click();
+        driver.findElements(cardInput).get(1).sendKeys(month);
+        driver.findElements(cardInput).get(2).click();
+        driver.findElements(cardInput).get(2).sendKeys(year);
+        driver.findElements(cardInput).get(3).click();
+        driver.findElements(cardInput).get(3).sendKeys(name);
+        return this;
+    }
+    public BillingPage verifyAddedCards(int expectedCount){
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h3[text()='Payment methods']"))));
+        Assert.assertEquals(driver.findElements(addedCards).size(), expectedCount);
+        return this;
+    }
+
+
+
 }
--- src/test/java/tests/BillingTest.java
+++ src/test/java/tests/BillingTest.java
@@ -1,4 +1,24 @@
 package tests;
 
-public class BillingTest {
+import models.User;
+import org.testng.annotations.Test;
+import pages.BillingPage;
+import pages.LogInPage;
+
+public class BillingTest extends BaseTest {
+
+    @Test
+    public void addCards(){
+        User user = new User("qwerty@mailinator.com", "12345678");
+        new LogInPage(driver)
+                .openPage()
+                .LogIn(user);
+        new BillingPage(driver)
+            .openPage()
+            .addNewCard()
+            .isPageOpened()
+            .fillFields("4242424242424242", "02", "2024", "Name Name")
+            .addClick()
+            .verifyAddedCards(3);
+    }
 }
